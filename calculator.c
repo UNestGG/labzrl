b0892c954bbb85ecef34b78ebf51491fa4c1921b
UNestGG <154469374+UNestGG@users.noreply.github.com>
2023-12-21 14:49:45 +0300

    Update calculator.c(delete comm)

--- calculator.c
+++ calculator.c
@@ -31,37 +31,5 @@ int calc(char *operator1, int a,int b) {
     } else if (*operator1 == '~') {
         return calc2(a);
     }
-	/*if(*operator ==  '+'){
-	    return a+b;
-        }
-        case '-':
-	    return a-b;
-            break;
-        case '*':
-            return a*b;
-	    break;
-        case '/':
-            if(b != 0) {
-                return a/b;
-            } else {
-                printf("Error: Division by zero");
-            }
-            break;
-        case '&':
-            return a&b;
-            break;
-        case '|':
-            return a|b;
-            break;
-        case '^':
-            return a^b;
-            break;
-        case '~':
-            calc2();
-            break;
-        default:
-            printf("Error: Invalid operator.\n");
-            break;
-       */
     return 0;
 }
