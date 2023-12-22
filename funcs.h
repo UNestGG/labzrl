ccd374a78e50ff0c5515626dda2bd75ca3ced3c1
UNestGG <154469374+UNestGG@users.noreply.github.com>
2023-12-22 13:02:03 +0300

    Update funcs.h

--- funcs.h
+++ funcs.h
@@ -3,7 +3,7 @@
 
 int transf_2(int n);
 int transf_8(int n);
-int transf_16(int n);
+void transf_16(int n);
 int calc2(int a);
 int calc(char *operator1, int a,int b);
 unsigned int customPower(unsigned int base, int exponent);
