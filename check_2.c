43310754f1d279bf4d6cb9ad00c261d9583c008b
UNestGG <154469374+UNestGG@users.noreply.github.com>
2023-12-22 19:38:54 +0300

    Update check_2.c

--- check_2.c
+++ check_2.c
@@ -4,14 +4,13 @@
 #include "funcs.h"
 
 int check2(char* n) {
-    char str[20];
-    sprintf(str,"%o", n);
-    char *endptr;
-    strtol(str, &endptr, 2);
-    if (*endptr == '0') {
-        return 2;
+    int len = strlen(n);
+    for (int i = 0; i < len; i++) {
+        if (n[i] != '0' && n[i] != '1') {
+            return 0;
+        }
     }
-    return 0;
+    return 2;
 }
 
 
